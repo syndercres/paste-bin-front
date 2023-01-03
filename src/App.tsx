2211852fcfab68d669923b5b4e756ed3cf5a0bce
syndercombecreswell <syndercombecreswell@gmail.com>
2023-01-03 14:24:04 +0000

    hello world

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { greet } from "./utils/greet";
 
 function App(): JSX.Element {
-  return <h1>{greet("World")}</h1>;
+  return <h1>Josiah and Sinbad pastebin pending</h1>;
 }
 
 export default App;
