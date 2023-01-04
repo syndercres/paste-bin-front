073973587530236cfe368e1d35f9712508130a7f
Josiah Adojutelegan <JosiahAdojutelegan@gmail.com>
2023-01-04 15:06:45 +0000

    Button placeholder

--- src/components/MainContent.tsx
+++ src/components/MainContent.tsx
@@ -105,6 +105,10 @@ export default function DisplayPasteBin():JSX.Element {
           );
         })}
       </div>
+
+
+{/*------------------------------------------------------------------------------Button to delete all entries */}
+
     </>
   );
 }
