0179413e911ec372fe2fe1a0a4e595e67b3940d1
syndercombecreswell <syndercombecreswell@gmail.com>
2023-01-04 12:41:08 +0000

    basic functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
-
+import DisplayPasteBin from "./components/MainContent";
 
 function App(): JSX.Element {
-  return <h1>Josiah and Sinbad pastebin pending</h1>;
+  return( 
+ 
+  <div>
+    <h1>Josiah and Sinbad pastebin pending</h1>
+    <DisplayPasteBin/>
+   </div>
+  
+  )
 }
 
 export default App;
--- src/components/MainContent.tsx
+++ src/components/MainContent.tsx
@@ -1,11 +1,88 @@
 import { useEffect, useState, ChangeEvent } from "react";
 import axios from "axios";
 
+export interface IPaste {
+    id: number;
+    name: string;
+    text: string;
+}
+
+const URL = "http://localhost:4001"
 
 export default function DisplayPasteBin(){
 
+    const [pasteList,setPasteList] = useState<IPaste[]>([])
+    const [pasteSubmit,setPasteSubmit] = useState({
+        name: "",
+        text: ""
+    })
 
 
-    
-return <div>hi there</div>
+
+
+
+const getPastesFromServer = async () => {
+    try{
+        const response = await axios.get(URL + "/pastes");
+
+        setPasteList(response.data.rows);
+  
+    } catch (error){
+        console.error(
+            "you have an error"
+        )
+    }
+   }
+
+const postPasteToServer = async (newName: string, newText: string) => {
+    try{
+        await axios.post(URL + "/paste", {name:newName, text:newText} )
+    } catch(error){
+        console.log("error from post")
+    }
+}
+
+   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    console.log("submitted", pasteSubmit)
+    postPasteToServer(pasteSubmit.name,pasteSubmit.text)
+
+}
+
+useEffect(() => {
+    getPastesFromServer();
+
+},[handleSubmit])
+
+return (
+<>
+<div>
+         <form onSubmit={handleSubmit}>
+
+         <input
+          placeholder="your name"
+          type= "text"
+          value={pasteSubmit.name}
+              onChange={(e) => setPasteSubmit({...pasteSubmit, name: e.target.value})}
+           />
+
+          <input
+          placeholder="paste here"
+          type= "text"
+          value={pasteSubmit.text}
+              onChange={(e) => setPasteSubmit({...pasteSubmit, text: e.target.value})}
+            />
+      <input type="submit" />
+        </form>
+        </div>
+
+    <div>
+    {pasteList.map((paste) => {
+        return (
+            <div key={paste.id}>{paste.name} {paste.text}</div>
+        )
+    })}
+</div>
+</>
+)
 }
