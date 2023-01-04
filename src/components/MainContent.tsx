f74b2026b6346d1de0c003f2ea4e2cc29e99299d
syndercombecreswell <syndercombecreswell@gmail.com>
2023-01-04 14:29:56 +0000

    removing warnings

--- src/components/MainContent.tsx
+++ src/components/MainContent.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState, ChangeEvent } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
 
 export interface IPaste {
@@ -9,7 +9,7 @@ export interface IPaste {
 
 const URL = "https://pastebin-server.onrender.com";
 
-export default function DisplayPasteBin() {
+export default function DisplayPasteBin(): JSX.Element {
   const [pasteList, setPasteList] = useState<IPaste[]>([]);
   const [pasteSubmit, setPasteSubmit] = useState({
     name: "",
@@ -38,11 +38,12 @@ export default function DisplayPasteBin() {
     e.preventDefault();
     console.log("submitted", pasteSubmit);
     postPasteToServer(pasteSubmit.name, pasteSubmit.text);
+    getPastesFromServer();
   };
 
   useEffect(() => {
     getPastesFromServer();
-  }, [handleSubmit]);
+  }, []);
 
   return (
     <>
